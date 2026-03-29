35f5d3082ea3a0e0e8cc858cee6ebdb33cc1477d
Sean Wortley <202449477+SeanWortley@users.noreply.github.com>
2026-03-29 12:19:24 +0200

    wip: bitboard operators

--- crates/engine_lib/src/core/bitboard.rs
+++ crates/engine_lib/src/core/bitboard.rs
@@ -1,13 +1,23 @@
 use crate::core::square::Square;
+use std::ops::{BitAnd, BitAndAssign, BitOr, BitOrAssign, Not};
 
 #[derive(Copy, Clone)]
 pub struct Bitboard(u64);
 
+impl BitAnd for Bitboard {
+    type Output = Bitboard;
+    fn bitand(self, other: Bitboard) -> Bitboard {
+        Bitboard(self.0 & other.0)
+    }
+}
+
 impl Bitboard {
+    pub const EMPTY: Bitboard = Bitboard(0);
+    pub const FULL: Bitboard = Bitboard(u64::MAX);
     pub fn bit(self) -> u64 {
         self.0
     }
-    pub fn empty(self) -> Self {
-        Bitboard(0)
+    pub fn from_square(square: Square) -> Self {
+        Bitboard(1 << square.index())
     }
 }
